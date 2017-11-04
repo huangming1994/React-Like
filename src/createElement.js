0dabef6ba424455e5b05d57dd5db19d4046a0360
huangming1994 <1157114498@qq.com>
2017-11-04 23:45:58 +0800

    创建函数式dom和jsx dom

---
+++ src/createElement.js
@@ -0,0 +1,65 @@
+function VNode(type, props, children) {
+  this.type = type
+  this.props = props
+  this.children = children
+}
+function createElement(type, props, ...args) {
+  let children = args.length > 0 ? args : null
+  return new VNode(type, props, children)
+}
+
+function mountText(text) {
+  return document.createTextNode(text)
+}
+
+function mountComposite(vnode) {
+  const node = vnode.type(vnode.props)
+  return mount(node)
+}
+
+function mountElement(vnode) {
+  const { type, props, children } = vnode
+  const node = document.createElement(type)
+  if (props) {
+    Object.keys(props).forEach(propName => {
+      node.setAttribute(propName, props[propName])
+    })
+  }
+  if (children) {
+    children.forEach(child => {
+      const childNode = mount(child)
+      node.appendChild(childNode)
+    })
+  }
+  return node
+}
+
+function mount(vnode) {
+  if (typeof vnode === 'string') {
+    return mountText(vnode)
+  }
+  if (typeof vnode.type === 'string') {
+    return mountElement(vnode)
+  }
+  if (typeof vnode.type === 'function') {
+    return mountComposite(vnode)
+  }
+}
+
+function renderByReact(vnode, container) {
+  const realNode = mount(vnode)
+  container.appendChild(realNode)
+  return realNode
+}
+
+function render(vnode, container) {
+  return renderByReact(vnode, container)
+}
+
+window.ReactDOM = {
+  render,
+}
+
+window.React = {
+  createElement,
+}
